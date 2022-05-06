fad667ff8582f59f8c2819a0e569610a8c00952e
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-05-06 05:46:30 +0300

    remove extra classes

--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -4,8 +4,8 @@ import s from './../Header.module.css'
 function Error404() {
     return (
         <div className={s.error_page}>
-            <div>404</div>
-            <div>Page not found!</div>
+            <div className={s.error_number}>404</div>
+            <div className={s.error_name}>Page not found!</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
         </div>
     )
