a188fee48d285142189713acfb21d50379d6a81e
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-08-10 22:28:13 +0300

    add:  reducers & logics to homeWorkReducer.ts

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,12 +1,26 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export type HomeWorkReducerType = SortUpAT | SortDownAT | CheckAT
+export type SortUpAT = {
+    type: 'sort-up',
+}
+export type SortDownAT = {
+    type: 'sort-down',
+}
+export type CheckAT = {
+    type: 'check',
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: HomeWorkReducerType): Array<UserType> => { // need to fix any
     switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
+        case 'sort-up': {
+            return [...state.sort((a,b) => a.name.localeCompare(b.name))]
+        }
+        case 'sort-down': {
+            return [...state.sort((a,b) => b.name.localeCompare(a.name))]
         }
         case 'check': {
-            // need to fix
-            return state
+            return [...state.filter((a) => a.age > 18)]
         }
         default: return state
     }
