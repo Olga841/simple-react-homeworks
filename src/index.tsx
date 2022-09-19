83e54b53ba1f7c07e5f15cbee15007b685ab8364
Olga Alekseeva <olgaalekseeva966@gmail.com>
2022-09-19 20:50:53 +0300

    add: import Provider from react-redux, store

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,18 @@ import './index.css'
 import App from './p1-main/u1-app/App'
 import * as serviceWorker from './serviceWorker'
 import {HashRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./p2-homeworks/h10/bll/store";
 
 
 // igg2
 
 const root = createRoot(document.getElementById('app') as Element)
 root.render(
+    <Provider store={store}>
         <App/>
+    </Provider>
+
 )
 
 serviceWorker.unregister()
